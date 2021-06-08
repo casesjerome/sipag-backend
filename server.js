c68f3e484497a427b93ca5b22a469b18a34b4208
Jerome Cases <casesjeromem@gmail.com>
2021-06-08 10:52:05 +0800

    Login routes

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 const log = console.log;
@@ -11,7 +17,15 @@ app.use(
     extended: true,
   })
 );
-
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -25,7 +39,7 @@ app.use(function (req, res, next) {
   next();
 });
 
-mongoose.connect("mongodb://localhost:27017/notesDB", {
+mongoose.connect("mongodb://localhost:27017/sipagDB", {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
@@ -35,17 +49,147 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const notesSchema = new mongoose.Schema({
-  title: String,
-  content: String,
-  user: userSchema
+  user: userSchema,
+  notes: { title: String, content: String },
 });
 
 const User = new mongoose.model("User", userSchema);
 const Note = new mongoose.model("Note", notesSchema);
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/form.html");
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//Routes
+//Dev Routes
+if (process.env.NODE_ENV !== "production") {
+  app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/notes.html");
+  });
+  app.get("/auth", (req, res) => {
+    res.sendFile(__dirname + "/auth.html");
+  });
+}
+
+//Register route
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
+
+  User.register({ username: username }, password, (err, user) => {
+    if (!err) {
+      passport.authenticate("local")(req, res, (err) => {
+        if (!err) {
+          const userData = {
+            userId: req.user._id,
+            username: req.user.username,
+          };
+          res.status(201).json({
+            status: 201,
+            data: userData,
+            message: "Successfuly Registered",
+          });
+        } else {
+          log(err);
+          res.status(500).json({
+            status: 500,
+            data: null,
+            message: "Internal server error",
+            error: err,
+          });
+        }
+      });
+    } else {
+      log(err);
+      res.status(400).json({
+        status: 400,
+        data: null,
+        message: "User already exists",
+        error: err,
+      });
+    }
+  });
+});
+
+//Login route
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  User.findOne(
+    {
+      username: username,
+    },
+    (err, foundUser) => {
+      if (!err) {
+        if (foundUser) {
+          const user = new User({
+            username: username,
+            password: password,
+          });
+          passport.authenticate("local")(req, res, async (err) => {
+            if (!err) {
+              await req.login(user, (err) => {
+                if (!err) {
+                  const userData = {
+                    userId: req.user._id,
+                    username: req.user.username,
+                  };
+                  res.status(200).json({
+                    status: 200,
+                    data: userData,
+                    message: "Successfuly Logged In",
+                  });
+                } else {
+                  log(err);
+                  res.status(401).json({
+                    status: 401,
+                    data: null,
+                    message: "Incorrect Password",
+                    error: err,
+                  });
+                }
+              });
+            } else {
+              log(err);
+              res.status(400).json({
+                status: 400,
+                data: null,
+                message: "Bad Request",
+                error: err,
+              });
+            }
+          });
+        } else {
+          log(err);
+          res.status(401).json({
+            status: 401,
+            data: null,
+            message: "Incorrect Username",
+            error: err,
+          });
+        }
+      } else {
+        log(err);
+        res.status(500).json({
+          status: 500,
+          data: null,
+          message: "Internal Server Error",
+          error: err,
+        });
+      }
+    }
+  );
+});
+
+//Logout route
+app.get("/logout", function (req, res) {
+  req.logout();
+  res.status(200).json({
+    status: 200,
+    data: userData,
+    message: "Successfuly Logout",
+  });
 });
 
 //Targeting all users
@@ -56,21 +200,8 @@ app
       if (!err) {
         res.send(foundUsers);
       } else {
-        res.send(err);
-      }
-    });
-  })
-  .post(async (req, res) => {
-    const user = new User({
-      username: req.body.username,
-      password: req.body.password,
-    });
-
-    await user.save((err) => {
-      if (!err) {
-        res.send("Successfully registered");
-      } else {
-        res.send(err);
+        log(err);
+        res.send("An error has occurred.");
       }
     });
   })
@@ -79,84 +210,89 @@ app
       if (!err) {
         res.send("Successfully deleted all users");
       } else {
-        res.send(err);
+        log(err);
+        res.send("An error has occurred.");
       }
     });
   });
 
 //Requests targeting a specific user
 app
-.route("/user/:userId")
-.get((req, res) => {
-  User.findOne(
-    {
-      _id: req.params.userId,
-    },
-    (err, foundUsers) => {
-      if (!err) {
-        if (foundUsers) {
-          res.send(foundUsers);
+  .route("/users/:userId")
+  .get((req, res) => {
+    User.findOne(
+      {
+        _id: req.params.userId,
+      },
+      (err, foundUsers) => {
+        if (!err) {
+          if (foundUsers) {
+            res.send(foundUsers);
+          } else {
+            res.send("No such user");
+          }
         } else {
-          res.send("No such user");
+          log(err);
+          res.send("An error has occurred.");
         }
-      } else {
-        res.send(err);
       }
-    }
-  );
-})
-.put((req, res) => {
-  User.update(
-    {
-      _id: req.params.userId,
-    },
-    {
-      username: req.body.username,
-      password: req.body.password,
-    },
-    {
-      overwrite: true,
-    },
-    (err) => {
-      if (!err) {
-        res.send("User updated");
-      } else {
-        res.send(err);
+    );
+  })
+  .put((req, res) => {
+    User.update(
+      {
+        _id: req.params.userId,
+      },
+      {
+        username: req.body.username,
+        password: req.body.password,
+      },
+      {
+        overwrite: true,
+      },
+      (err) => {
+        if (!err) {
+          res.send("User updated");
+        } else {
+          log(err);
+          res.send("An error has occurred.");
+        }
       }
-    }
-  );
-})
-.patch((req, res) => {
-  User.update(
-    {
-      _id: req.params.userId,
-    },
-    {
-      $set: req.body,
-    },
-    (err) => {
-      if (!err) {
-        res.send("User updated");
-      } else {
-        res.send(err);
+    );
+  })
+  .patch((req, res) => {
+    User.update(
+      {
+        _id: req.params.userId,
+      },
+      {
+        $set: req.body,
+      },
+      (err) => {
+        if (!err) {
+          res.send("User updated");
+        } else {
+          log(err);
+          res.send("An error has occurred.");
+        }
       }
-    }
-  );
-})
-.delete((req, res) => {
-  User.deleteOne(
-    {
-      _id: req.params.userId,
-    },
-    (err) => {
-      if (!err) {
-        res.send("User deleted");
-      } else {
-        res.send(err);
+    );
+  })
+  .delete((req, res) => {
+    User.deleteOne(
+      {
+        _id: req.params.userId,
+      },
+      (err) => {
+        if (!err) {
+          res.send("User deleted");
+        } else {
+          log(err);
+          res.send("An error has occurred.");
+        }
       }
-    }
-  );
-});
+    );
+  });
 
 //Requests targeting all notes
 app
@@ -166,7 +302,8 @@ app
       if (!err) {
         res.send(foundNotes);
       } else {
-        res.send(err);
+        log(err);
+        res.send("An error has occurred.");
       }
     });
   })
@@ -180,7 +317,8 @@ app
       if (!err) {
         res.send("Successfully added a new note");
       } else {
-        res.send(err);
+        log(err);
+        res.send("An error has occurred.");
       }
     });
   })
@@ -189,7 +327,46 @@ app
       if (!err) {
         res.send("Successfully deleted all notes.");
       } else {
-        res.send(err);
+        log(err);
+        res.send("An error has occurred.");
+      }
+    });
+  });
+
+//Requests targeting all notes on a specific user
+app
+  .route("/:userId/notes")
+  .get((req, res) => {
+    Note.findOne(
+      {
+        _id: req.params.userId,
+      },
+      (err, foundNotes) => {
+        if (!err) {
+          if (foundNotes) {
+            res.send(foundNotes);
+          } else {
+            res.send("No such note");
+          }
+        } else {
+          log(err);
+          res.send("An error has occurred.");
+        }
+      }
+    );
+  })
+  .post(async (req, res) => {
+    const note = new Note({
+      title: req.body.title,
+      content: req.body.content,
+    });
+
+    await note.save((err) => {
+      if (!err) {
+        res.send("Successfully added a new note");
+      } else {
+        log(err);
+        res.send("An error has occurred.");
       }
     });
   });
@@ -210,7 +387,8 @@ app
             res.send("No such note");
           }
         } else {
-          res.send(err);
+          log(err);
+          res.send("An error has occurred.");
         }
       }
     );
@@ -231,7 +409,8 @@ app
         if (!err) {
           res.send("Note updated");
         } else {
-          res.send(err);
+          log(err);
+          res.send("An error has occurred.");
         }
       }
     );
@@ -248,7 +427,8 @@ app
         if (!err) {
           res.send("Note updated");
         } else {
-          res.send(err);
+          log(err);
+          res.send("An error has occurred.");
         }
       }
     );
@@ -262,7 +442,8 @@ app
         if (!err) {
           res.send("Note deleted");
         } else {
-          res.send(err);
+          log(err);
+          res.send("An error has occurred.");
         }
       }
     );
